bdf45282a04f37a9593fe3307bca3bfeb8ee9494
anaspits <43137706+anaspits@users.noreply.github.com>
2018-09-15 20:06:49 +0300

    Update Calculator.java
    Hey,
    I added the sin,tan,cos buttons and I changed the way the buttons appear on the layout by adding some panels, so it will look more organised. I also fixed the size of the window so it can't be resizable, because then the buttons will change place. I hope you find my changes useful :)

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -12,9 +12,17 @@ public class Calculator extends JFrame {
 	private static double result;
 	private static double copyBuffer;
 	
+	private JPanel panel1;
+        private JPanel panel2;
+        private JPanel panel3;
+        private JPanel panel4;
+        private JPanel panel5;
+        private JPanel panel6;
+	
 	public Calculator() {
 		JFrame main = new JFrame("Calculator");
 		main.setSize(320, 265);
+		main.setResizable(false);
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.setLayout(new FlowLayout());
 		JLabel output = new JLabel("");
@@ -45,29 +53,55 @@ public class Calculator extends JFrame {
 		JButton point = new JButton(".");
 		JButton copy = new JButton("Copy");
 		JButton paste = new JButton("Paste");
+		JButton sinButton = new JButton("sin");
+                JButton cosButton = new JButton("cos");
+                JButton tanButton = new JButton("tan");
 		JLabel error = new JLabel("");
 		error.setPreferredSize(new Dimension(300, 25));
 		error.setForeground(Color.RED);
 		main.add(outputbox);
-		main.add(clear);
-		main.add(copy);
-		main.add(paste);
-		main.add(oneButton);
-		main.add(twoButton);
-		main.add(threeButton);
-		main.add(addition);
-		main.add(fourButton);
-		main.add(fiveButton);
-		main.add(sixButton);
-		main.add(subtraction);
-		main.add(sevenButton);
-		main.add(eightButton);
-		main.add(nineButton);
-		main.add(multiplication);
-		main.add(zeroButton);
-		main.add(point);
-		main.add(division);
-		main.add(equals);
+		panel1 = new JPanel();
+                panel2 = new JPanel();
+                panel3 = new JPanel();
+                panel4 = new JPanel();
+                panel5 = new JPanel();
+                panel6 = new JPanel();
+  		main.add(panel1);
+                main.add(panel2);
+                main.add(panel3);
+                main.add(panel4);
+                main.add(panel5);
+                main.add(panel6);
+		panel1.setLayout(new GridLayout(1, 5, 2, 2));
+                panel2.setLayout(new GridLayout(1, 5, 2, 2));
+                panel3.setLayout(new GridLayout(1, 5, 2, 2));
+                panel4.setLayout(new GridLayout(1, 5, 2, 2));
+                panel5.setLayout(new GridLayout(1, 5, 2, 2));
+                panel6.setLayout(new GridLayout(1, 5, 2, 2));
+
+		panel1.add(clear);
+		panel1.add(copy);
+		panel1.add(paste);
+		panel2.add(oneButton);
+		panel2.add(twoButton);
+		panel2.add(threeButton);
+		panel2.add(addition);
+		panel3.add(fourButton);
+		panel3.add(fiveButton);
+		panel3.add(sixButton);
+		panel3.add(subtraction);
+		panel4.add(sevenButton);
+		panel4.add(eightButton);
+		panel4.add(nineButton);
+		panel4.add(multiplication);
+		panel5.add(point);
+                panel5.add(zeroButton);
+		panel5.add(division);
+		panel5.add(equals);
+                panel6.add(sinButton);
+                panel6.add(cosButton);
+                panel6.add(tanButton);
+		
 		main.add(error);
 		main.setVisible(true);
 		
@@ -170,6 +204,65 @@ public class Calculator extends JFrame {
 				}
 			}
 		});		
+		 sinButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					action = "sin";
+					num1 = Double.parseDouble(output.getText());
+					result = Math.sin(num1);
+					num1Label.setText("");
+					operation.setText("");
+					output.setText(Double.toString(result));
+					num1 = Double.parseDouble(output.getText());
+					//num2 = 0;
+				}
+				catch (Exception ae) {
+					error.setText("You must enter a number");
+				}
+			}
+		});
+                
+                cosButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					
+					num1 = Double.parseDouble(output.getText());
+					result = Math.cos(num1);
+					num1Label.setText("");
+					operation.setText("");
+					output.setText(Double.toString(result));
+					num1 = Double.parseDouble(output.getText());
+					//num2 = 0;
+				}
+				catch (Exception ae) {
+					error.setText("You must enter a number");
+				}
+			}
+		});
+                tanButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					
+					num1 = Double.parseDouble(output.getText());
+					result = Math.tan(num1);
+					num1Label.setText("");
+					operation.setText("");
+					output.setText(Double.toString(result));
+					num1 = Double.parseDouble(output.getText());
+					//num2 = 0;
+				}
+				catch (Exception ae) {
+					error.setText("You must enter a number");
+				}
+			}
+		});
+                
 		subtraction.addActionListener(new ActionListener() {
 
 			@Override
